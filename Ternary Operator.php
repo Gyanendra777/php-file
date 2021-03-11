adc8dbae89f7cd0ff61217fa19d028f0b27977f8
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-11 15:49:12 +0530

    Ternary Operator

---
+++ Ternary Operator.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+ 
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  $x = 30;
+  $y = 30;
+  $z = ($x < 20)? "raju":"dubey";
+  $c = ($y < 40 ? "raju":"dubey");
+  echo $z."<br>";
+  echo $c;
+      
+  ?>
+</body>
+</html>
