39ea6a304ce61bc18adb3ba3ebdc496812ec8d47
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-11 14:58:06 +0530

    if statement

---
+++ If Statement.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+ 
+  <title>Document</title>
+</head>
+<body>
+  <?php
+        $a = 30;      
+        $b = 300;
+        if ($a < $b) {
+            echo "a is smaller <br>";
+        }      
+        echo " here is other statement";
+  ?>
+</body>
+</html>
