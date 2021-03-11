e0289ccf4ebb6bf21c74b26ec0997bee4f76197e
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-11 14:50:25 +0530

    comparison Operators

---
+++ Comparison Operators.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+ 
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  $c = 10;
+  $ab = "10";
+      echo $c === $ab;
+  $a = 10;
+  $b = 10;
+      echo $a === $b;
+  ?>
+</body>
+</html>
