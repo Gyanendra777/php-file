0ad9503cb23f241db54eaedb8e11c6680037a70a
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-11 17:43:30 +0530

    Goto Statement

---
+++ Goto Statement .php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+ 
+  <title>Document</title>
+</head>
+<body>
+  <?php
+      echo "1 hello goto statement <br>";
+      echo "2 hello goto statement <br>";
+      goto abc;
+
+      echo "3 hello goto statement <br>";
+      echo "4 hello goto statement <br>";
+      echo "5 hello goto statement <br>";
+      echo "6 hello goto statement <br>";
+
+      abc:
+      echo "7 hello goto statement <br>";
+      echo "8 hello goto statement <br>";
+  ?>
+</body>
+</html>
