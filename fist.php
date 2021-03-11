b558e8a3a1f9c33f94e5efde34f3547e55002585
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-11 12:26:02 +0530

    Echo & Print Statement

--- fist.php
+++ fist.php
@@ -1,3 +1,30 @@
-<?php 
-echo "hello word"
-?>
+
+
+
+<html>
+    <head> 
+      <title>php</title>
+    </head>
+    <body>
+          <?php 
+          // echo statement 
+          echo "hello word php and start";
+          echo 'hello<br>';
+          echo "<b>hello<b><br>";
+          echo "gyanendra","dubey<br>";
+          echo "gyanendra"."dubey<br>";
+          echo ("hello <br>");
+          echo 23.34;
+          echo "++++++++++------++++++++++<br>";
+
+          //print statement
+          print "helo print-------------<br>";
+          print ("print-------------<br>");
+          print "gyanendra print------------- <br>";
+          print "<h1>gyanendra dubey print-------------<h1><br>";
+          print 23.34;
+
+
+          ?>
+    </body>
+</html>
