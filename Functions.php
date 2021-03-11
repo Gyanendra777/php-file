e1a47b89f1eae9afcc58c0fc56c5f9766056df57
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-11 17:59:33 +0530

    FunctionName

---
+++ Functions.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+ 
+  <title>Document</title>
+</head>
+<body>
+  <?php
+       function FunctionName(Type $var = null)
+      {
+        echo "function coll <br>";
+        # code...
+      }
+      FunctionName();
+      FunctionName();
+      FunctionName();
+      FunctionName();
+  ?>
+</body>
+</html>
