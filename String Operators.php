e2d364b21ac7d40f3ae43946700c970ff6cf3a5a
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-11 16:38:08 +0530

    String Operators

---
+++ String Operators.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+ 
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  $a = 200 ;
+  $b = $a ."hello ";
+  echo $b."<br>";
+
+  $x = 200;
+  $x .= "hello";
+  $x .= "---";
+  echo $x;
+      
+  ?>
+</body>
+</html>
