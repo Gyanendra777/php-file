5ed1223eca1ad49897a69069ed58ea5f76ba166f
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-11 17:22:20 +0530

    Nested Loop

---
+++ Nested Loop.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+ 
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    for ($x=0; $x < 10; $x++) { 
+      echo $x."=";
+      for ($a=1; $a < 5; $a++) { 
+        echo $a.")hello";
+      }
+      
+      echo "<br>";
+    }
+    
+      
+  ?>
+</body>
+</html>
