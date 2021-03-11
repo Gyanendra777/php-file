e0d552b8e8aba538aef436672954697d3f7d8c31
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-11 17:03:55 +0530

    For Loop

---
+++ For Loop.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+ 
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  for ($x=0; $x < 10; $x++) { 
+    echo $x.")hello worde <br>";
+  }
+      
+  ?>
+</body>
+</html>
