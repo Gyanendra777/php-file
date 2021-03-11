ea9cc6dc84952a6f2e7629e8c90684335978e833
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-11 14:44:08 +0530

    Assignment Operators

---
+++ Assignment Operators.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+ 
+  <title>Document</title>
+</head>
+<body>
+  <?php
+      $a = 10;
+      $b = 2;
+
+      // $a -= $b;
+
+      // $a += $b;
+
+      // $a /= $b;
+
+      $a *= $b;
+      $a **= $b;
+
+      echo $a;
+  ?>
+</body>
+</html>
