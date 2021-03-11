863dfd3cee87831dbc31f9ff89c470988a15a0ec
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-11 14:03:57 +0530

    variable Tutorial

---
+++ Variables Tutorial.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+  <title>Variables Tutorial</title>
+</head>
+<body>
+  <?php
+    $titel = "dubey";
+    $tite = 123;
+    $titee = array("a","b","c");
+    $tites = null;
+    
+    echo "<h1>gyanendra . $tite </h1>";
+    echo "<h1>gyanendra . $titel </h1>";
+    echo "<h1>gyanendra .$titee[1] </h1>";
+    
+    var_dump ($tite)
+  ?>
+</body>
+</html>
