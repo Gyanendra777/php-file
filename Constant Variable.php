20b208a67a531043eb2b9b0ab4750adc4419afe5
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-11 14:26:32 +0530

    variable

---
+++ Constant Variable.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+ 
+  <title>Document</title>
+</head>
+<body> 
+  <?php
+  // variable php
+     define("text", 40);
+    //  define("text", 2);
+     echo text; 
+  ?>
+</body>
+</html>
