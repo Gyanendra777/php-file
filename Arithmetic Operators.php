19cf09e940a0df359328bbc2e0f855fd4dc4c172
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-11 14:37:45 +0530

    Arithmetic Operators

---
+++ Arithmetic Operators.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+ 
+  <title>Document</title>
+</head>
+<body>
+  <?php
+      $a = 10;
+      $b = 3;
+      $c = $a ** $b;
+      $c = ($a + $b) * 4;
+      $a++;
+      ++$a;
+      $a--;
+      --$a;
+      echo $c;
+  ?>
+</body>
+</html>
