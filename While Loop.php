298542a76fd666418ec4cd6843a255ef1b30a815
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-11 16:51:14 +0530

    While Loop

---
+++ While Loop.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+ 
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  $a = 1;
+  while($a < 10){
+    echo "<li>".$a.")hello .....<br>";
+    $a++;
+  }
+      
+  ?>
+</body>
+</html>
